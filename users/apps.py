c5044e90f6b14f2c3f46eb8ef1a72b01e174eb66
joaopedro117 <joaopedrodesouza789@gmai.com>
2025-03-25 23:59:46 -0300

    Comentário no arquivo

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+ # Fiz alteração, vou digitar os seguintes comandos
+ #tlgd tem alguns codigos por ai com comentario kkkk
+ # Logo após, comenta a alteração do arquivo com o
+
+ #salvar comando...(git status)
